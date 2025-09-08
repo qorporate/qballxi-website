a192a471157fe00ca87d29fbee0e98e53711830d
henrychris <64085210+henrychris@users.noreply.github.com>
2025-09-08 15:38:18 +0100

    Update ProductContainer.svelte

--- src/lib/components/ProductContainer.svelte
+++ src/lib/components/ProductContainer.svelte
@@ -14,7 +14,7 @@
 		class="relative h-full w-full rounded-3xl border-1 border-gray-300"
 		style="background-image: url({imageSrc}); background-size: cover; background-position: 50%;"
 	>
-		<div class="absolute bottom-0 h-[35%] w-full rounded-3xl bg-white p-6">
+		<div class="absolute bottom-0 h-[35%] w-full rounded-3xl bg-white p-6 hover:h-full hover:bg-blue-400">
 			<h1>{title}</h1>
 			<p>{description}</p>
 		</div>
