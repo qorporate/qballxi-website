f43a39665120698831a8d112a4a9e09785edc73f
henrychris <64085210+henrychris@users.noreply.github.com>
2025-09-08 18:07:51 +0100

    1200 x 630

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const COMPANY_NAME = 'Qball Core';
 export const QBALL_LOGO =
-	'https://res.cloudinary.com/dit0zbles/image/upload/t_to_webp/v1743166642/qball-corp/logos/qball-logo.webp';
+	'https://res.cloudinary.com/dit0zbles/image/upload/t_opengraph/qball-corp/logos/qball-logo';
