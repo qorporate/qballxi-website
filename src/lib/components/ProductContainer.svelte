e562ca2f688a112424fe11d1fd24eb3ce1d73d41
henrychris <64085210+henrychris@users.noreply.github.com>
2025-09-08 14:24:09 +0100

    fix styling

--- src/lib/components/ProductContainer.svelte
+++ src/lib/components/ProductContainer.svelte
@@ -9,12 +9,12 @@
 	const { imageSrc, title, description, href }: Props = $props();
 </script>
 
-<div class="min-h-[500px] cursor-pointer rounded-4xl border-1 border-gray-300 p-2">
+<div class="h-[500px] cursor-pointer rounded-4xl border-1 border-gray-300 p-2">
 	<div
-		class="h-full w-full rounded-3xl border-1 border-gray-300"
+		class="relative h-full w-full rounded-3xl border-1 border-gray-300"
 		style="background-image: url({imageSrc}); background-size: cover; background-position: 50%;"
 	>
-		<div class="flex h-[35%] w-full flex-col justify-between rounded-3xl bg-white p-6">
+		<div class="absolute bottom-0 h-[35%] w-full rounded-3xl bg-white p-6">
 			<h1>{title}</h1>
 			<p>{description}</p>
 		</div>
