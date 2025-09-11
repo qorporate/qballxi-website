404491adb4901dcbc75884985007df6921536c27
henrychris <64085210+henrychris@users.noreply.github.com>
2025-09-11 18:56:46 +0100

    use qset posthog key

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,7 +3,7 @@ import posthog from 'posthog-js';
 
 export const load = async () => {
 	if (browser && !dev) {
-		posthog.init('phc_yVKdkuhFl3UwKvoyYr2GIp2Nx1vNnTIzRer9ZCV3teV', {
+		posthog.init('phc_LhQCeUx9IuQYYxJ5W8wtTI4BzUVHNuDlq9V2DKlrSRZ', {
 			api_host: 'https://us.i.posthog.com',
 			defaults: '2025-05-24',
         	person_profiles: 'always',
